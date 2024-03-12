80047d438e5c19180c3d954cf5aaf69f537d6942
Gabriel Spadon <gabriel@spadon.com.br>
2024-03-12 18:28:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,32 +7,32 @@ from numpy.distutils.core import setup
 try:
     import numpy
 except ImportError:
-    print('numpy is required during installation')
+    print("numpy is required during installation")
     sys.exit(1)
 
 
-DISTNAME = 'Soft-DTW-TF-Keras'
-DESCRIPTION = "Soft-DTW Loss Function implementation for Keras Tensorflow "
-LONG_DESCRIPTION = ''
-MAINTAINER = 'Jay Kumar'
-MAINTAINER_EMAIL = ''
-URL = 'https://github.com/JayKumarr/Soft-DTW-TF-Keras'
-LICENSE = ''
-DOWNLOAD_URL = 'https://github.com/JayKumarr/Soft-DTW-TF-Keras'
-VERSION = '0.1.dev0'
+DISTNAME = "Soft-DTW"
+DESCRIPTION = "Soft-DTW loss function for Keras/TensorFlow"
+LONG_DESCRIPTION = ""
+MAINTAINER = "AISViz"
+MAINTAINER_EMAIL = "aisviz@dal.ca"
+URL = "https://github.com/aisviz/Soft-DTW"
+LICENSE = "https://github.com/aisviz/Soft-DTW/LICENSE"
+DOWNLOAD_URL = "https://github.com/JayKumarr/Soft-DTW"
+VERSION = "1.0.0"
 
 
-def configuration(parent_package='', top_path=None):
+def configuration(parent_package="", top_path=None):
     from numpy.distutils.misc_util import Configuration
 
     config = Configuration(None, parent_package, top_path)
 
-    config.add_subpackage('softdtwkeras')
+    config.add_subpackage("softdtwkeras")
 
     return config
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     old_path = os.getcwd()
     local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
 
@@ -52,13 +52,18 @@ if __name__ == '__main__':
           long_description=LONG_DESCRIPTION,
           zip_safe=False,  # the package can run out of an .egg file
           classifiers=[
-              'Intended Audience :: Science/Research',
-              'Intended Audience :: Developers', 'License :: OSI Approved',
-              'Programming Language :: C', 'Programming Language :: Python',
-              'Topic :: Software Development',
-              'Topic :: Scientific/Engineering',
-              'Operating System :: Microsoft :: Windows',
-              'Operating System :: POSIX', 'Operating System :: Unix',
-              'Operating System :: MacOS'
-             ]
-          )
+              "Intended Audience :: Developers",
+              "Intended Audience :: Science/Research",
+              "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+              "Operating System :: MacOS" "Operating System :: POSIX :: Linux",
+              "Operating System :: Microsoft :: Windows",
+              "Operating System :: Unix",
+              "Programming Language :: C",
+              "Programming Language :: Python :: 3.10",
+              "Programming Language :: Python :: 3.8",
+              "Programming Language :: Python :: 3.9",
+              "Topic :: Scientific/Engineering :: Information Analysis",
+              "Topic :: Software Development",
+              "Topic :: Utilities",
+        ]
+    )
