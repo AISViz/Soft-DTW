2a63fcb753f3e16f4b39d0865f5e9d0877664a4b
Jay-PC <Jay_Tharwani1992@yahoo.com>
2024-03-13 15:07:50 -0300

    updating testing_sdtw.py and test in actionflow

--- tests/testing_sdtw.py
+++ tests/testing_sdtw.py
@@ -1,23 +1,18 @@
 import numpy as np
-from sdtw import SoftDTW, distance as sdtw_distance
-
-
+from tslearn.metrics import SquaredEuclidean, SoftDTW
 import tensorflow as tf
-
 from softdtwkeras.SDTWLoss import SDTWLoss
 
 if __name__ == '__main__':
     x_ = np.array([[0,0], [0.9,0], [0,0], [0.5,1], [0,0], [0,0]])
     y_ = np.array([[0,0], [0,0], [0.1,0], [0.9,1], [0,0], [0.5,0]])
 
+
     x_tf = tf.convert_to_tensor(x_, dtype=tf.float64)
     y_tf = tf.convert_to_tensor(y_, dtype=tf.float64)
 
-    D = sdtw_distance.SquaredEuclidean(x_, y_)
 
-    tff_sdtw__= SDTWLoss(gamma=0.001)
-    val_tensor = tff_sdtw__.unit_loss(x_tf, y_tf)
-
+    D = SquaredEuclidean(x_, y_)
     sdtw = SoftDTW(D, gamma=0.001)
     # soft-DTW discrepancy, approaches DTW as gamma -> 0
     value = sdtw.compute()
@@ -25,6 +20,11 @@ if __name__ == '__main__':
     E = sdtw.grad()
     # gradient w.r.t. X, shape = [m, d]
     G = D.jacobian_product(E)
+
+    tff_sdtw__= SDTWLoss(gamma=0.001)
+    val_tensor = tff_sdtw__.unit_loss(x_tf, y_tf)
+
+
 
     print(value)
     print(val_tensor)
